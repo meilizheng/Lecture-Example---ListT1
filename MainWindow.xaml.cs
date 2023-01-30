5a877907c76554c82da55371166dfa2f4020da8f
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-01-30 13:28:53 -0800

    added and fixed code
    added and fixed code.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//Meili Zheng;
+//Lecture Example - List<T>
+//01/30/2023;
+
 
 namespace Lecture_Example___ListT
 {
@@ -20,9 +24,112 @@ namespace Lecture_Example___ListT
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> namesList = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        public void DisplayFromList()
+        {
+            rtbdisplay1.Text = "";
+            for (int i = 0; i < namesList.Count; i++)
+            {
+                FormatString(i);
+            }           
+        }
+
+        public void FormatString(int i)
+        {
+            rtbdisplay1.Text += $"{i}  {namesList[i]} \n";
+        }
+
+        private void bntAdd_Click(object sender, RoutedEventArgs e)
+        {                           
+            string addtolist = txtAdd.Text;
+            namesList.Add(addtolist);            
+            DisplayFromList();            
+        }
+
+        private void bntDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            int number = 0;
+            string inputIndex = txtDisplaybyindex.Text;
+            bool inputindex = int.TryParse(inputIndex, out number);
+            if (inputindex)
+            {
+                int inputnumber = int.Parse(txtDisplaybyindex.Text);
+
+                for (int i = 0; i < namesList.Count; i++)
+                {
+                    if (i == inputnumber)
+                    {
+                        rtbdisplay1.Text = i + "  " + namesList[i];
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invaid information");
+            }            
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {          
+            
+                int number = 0;
+                string inputIndex = txtRemove.Text;
+                bool inputindex = int.TryParse(inputIndex, out number);
+            if (inputindex)
+            {
+                int removeIndex = int.Parse(txtRemove.Text);
+
+                for (int i = 0; i < namesList.Count; i++)
+                {
+                    if (i == removeIndex)
+                    {
+                        namesList.RemoveAt(i);
+                        DisplayFromList();
+                    }
+                }
+            }              
+            else
+            {
+               MessageBox.Show("Invaid information");
+            }                         
+        }
+
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            string removeItem = txtRemoveItem.Text;
+            for (int i = 0; i < namesList.Count; i++)
+            {
+                if (removeItem == namesList[i])
+                {
+                    namesList.RemoveAt(i);
+                    DisplayFromList();
+                }
+            }
+        }
+
+        private void btnRemoveRange_Click(object sender, RoutedEventArgs e)
+        {
+            int remoVevalue1 = int.Parse(txtRemovevalue1.Text);
+            int removeValue2 = int.Parse(txtRemovevalue2.Text);
+            for (int i = 0; i < namesList.Count; i++)
+            {                
+                if (i >= remoVevalue1 && i <= removeValue2)
+                {
+                    namesList.RemoveAt(i); ;
+                    DisplayFromList();
+                }                                 
+            }     
+          }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            rtbdisplay1.Text = "";
         }
     }
 }
